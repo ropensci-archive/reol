8e9757db8fdc4081fab28a4a7b0a69b5f908d669
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-22 21:47:53 -0700

    added missing ... param def for pageprocessing fxn

--- R/PageProcessing.R
+++ R/PageProcessing.R
@@ -1,21 +1,22 @@
 #' Process XML Data into tree format
-#' 
+#'
 #' This function will read in the XML data and parse it into a tree structure for R to read.
-#'   
+#'
 #' @export
-#' 
+#'
 #' @param MyEOL A filename or R object for downloaded EOL pages
 #' @param MyFiles A vector of filenames or R objects for EOL  or Hier pages
-#'   
-#' @return \code{PageProcessing} returns XML tree as an R object. \code{RemoveNAFiles} is an 
-#' internal function that will take a vector of filenames and check to make sure they all have 
-#' data associated (sometimes requested pages will be empty and contain no information).  If they 
+#' @param ... Parameters passed on to \code{\link[XML]{xmlRoot}}
+#'
+#' @return \code{PageProcessing} returns XML tree as an R object. \code{RemoveNAFiles} is an
+#' internal function that will take a vector of filenames and check to make sure they all have
+#' data associated (sometimes requested pages will be empty and contain no information).  If they
 #' are empty, they are cleaved from the analysis.
-#'   
+#'
 #' @examples \dontrun{
 #' # Reads in a file
 #' # PageProcessing("eol1.xml")
-#' 
+#'
 #' # or reads in an R object
 #' data(MyEOLs)
 #' PageProcessing(MyEOLs[1])
